dc49a30d1635bc1ddee995f3ea4b23e05172989b
Christopher Zorn <tofu@thetofu.com>
2008-09-16 18:46:36 +0000

    importing punjab, no need for history

---
+++ tests/xep206.py
@@ -0,0 +1,29 @@
+
+import os
+import sys, sha
+from twisted.trial import unittest
+import time
+from twisted.words.protocols.jabber import jid
+from twisted.internet import defer, protocol, reactor
+from twisted.application import internet, service
+from twisted.words.xish import domish, xpath
+
+from twisted.python import log
+
+class DummyClient:
+    """
+    a client for testing
+    """
+
+class DummyTransport:
+    """
+    a transport for testing
+    """
+
+
+
+class XEP0206TestCase(unittest.TestCase):
+    """
+    Tests for Punjab compatability with http://www.xmpp.org/extensions/xep-0206.html
+    """
+
---
+++ twisted/plugins/punjab.py
@@ -0,0 +1,8 @@
+
+from twisted.scripts.mktap import _tapHelper
+ 	
+Punjab = _tapHelper(
+ 	        "Punjab",
+                "punjab.tap",
+ 	        "A HTTP XMPP client interface",
+                "punjab")
